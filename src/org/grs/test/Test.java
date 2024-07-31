d7ae6ca40d74296bbbfde64cb5fc3a0c9fb49e59
gowtham-yg <gowtham8783@gmail.com>
2024-07-31 16:52:50 +0530

    New

--- src/org/grs/test/Test.java
+++ src/org/grs/test/Test.java
@@ -2,18 +2,18 @@ package org.grs.test;
 
 public class Test 
 {
+	
 public static void main(String[] args) 
 {
   System.out.println("Hello world");	
 }
-<<<<<<< HEAD
 private void addToKart() 
 {
 	System.out.println("This is Ramakrishnan - working on addtokart");
-=======
+}
 private void payment() 
 {
  System.out.println("This is Gowtham - working on payment module");
->>>>>>> bbe2632bb4027c85cd872e06cfba814b7758c1ca
 }
+
 }
